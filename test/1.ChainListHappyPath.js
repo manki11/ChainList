434088d417046bcc3a5b28a1e94093f8a3c503c2
Mankirat Singh <mankirat1998@gmail.com>
2017-11-02 20:42:47 +0530

    feat: changes made in contract and test for frontend

--- test/1.ChainListHappyPath.js
+++ test/1.ChainListHappyPath.js
@@ -19,10 +19,14 @@ contract('ChainList', function(accounts) {
     // Test case: check initial values
     it("should be initialized with empty values", function() {
         return ChainList.deployed().then(function(instance) {
-            return instance.getNumberOfArticles();
+            chainListInstance= instance;
+            return chainListInstance.getNumberOfArticles();
         }).then(function(data) {
             assert.equal(data, 0x0, "number of articles must be zero");
-        });
+            return chainListInstance.getArticlesForSale();
+        }).then(function (data) {
+            assert.equal(data.length, 0,"articles for sale must be empty");
+        })
     });
 
     // Test case: sell a first article
